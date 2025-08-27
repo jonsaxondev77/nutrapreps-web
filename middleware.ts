f1d28d3e2af29c3a7572a9af216708ceacd5b037
jonsaxondev77 <saxonjonathan6@gmail.com>
2025-08-27 21:45:30 +0100

    Debug

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,8 @@ export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith('/order')) {
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
+    console.log(token);
+
     // Redirect to signin if user is not logged in or not active
     if (!token) {
       const url = req.nextUrl.clone();
