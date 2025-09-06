4164364c69726c59cc68ff5177fbe004e9cc8814
jonsaxondev77 <saxonjonathan6@gmail.com>
2025-09-06 20:44:34 +0100

    Updated packages api

--- lib/store/services/packagesApi.ts
+++ lib/store/services/packagesApi.ts
@@ -1,19 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { Package } from '@/types/ordering';
 import { getSession } from 'next-auth/react';
+import { baseQueryWithRedirect } from './baseQuery';
 
 export const packageApi = createApi({
   reducerPath: 'packageApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_URL,
-    prepareHeaders: async (headers) => {
-      const session = await getSession();
-      if (session?.user.jwtToken) {
-        headers.set("Authorization", `Bearer ${session.user.jwtToken}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithRedirect,
   endpoints: (builder) => ({
     getPackages: builder.query<Package[], void>({
       query: () => 'packages/all',
